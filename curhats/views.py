7cc8bbb35f3473f08ae5a56a30b4b4a25b4e1c6d
prayogitio <tio.qynxx@gmail.com>
2018-03-07 13:00:34 +0700

    added curhat page, can post and view posts

--- curhats/views.py
+++ curhats/views.py
@@ -4,11 +4,18 @@ from . import forms
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url="accounts/login")
+@login_required(login_url="/accounts/login")
 def curhat_list(request):
     post_form = forms.CreatePost(request.POST)
-    return render(request, 'curhats/curhat_page.html', { 'post_form':post_form })
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'curhats/curhat_page.html', { 'post_form':post_form, 'post_data':posts })
 
-@login_required(login_url="accounts/login")
+@login_required(login_url="/accounts/login")
 def create_curhat(request):
-    pass
+    if request.method == 'POST':
+        form = forms.CreatePost(request.POST)
+        if form.is_valid():
+            get_instance = form.save(commit=False)
+            get_instance.author = request.user
+            get_instance.save()
+            return redirect('curhats:list')
