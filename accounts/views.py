e607329dcf0152402a10a83460d9c84a00f08b93
prayogitio <tio.qynxx@gmail.com>
2018-03-09 16:21:22 +0700

    form account made

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.template.context_processors import csrf
+from . import forms
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = forms.MyLogin(data=request.POST)
         if form.is_valid():
             #log in the user
             user = form.get_user()
@@ -15,20 +16,20 @@ def login_view(request):
             else:
                 return redirect('curhats:list')
     else:
-        form = AuthenticationForm()
-    return render(request, 'index.html', { 'login_form':form, 'regis_form':UserCreationForm() })
+        form = forms.MyLogin()
+    return render(request, 'index.html', { 'login_form':form, 'regis_form':forms.MyRegis() })
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = forms.MyRegis(request.POST)
         if form.is_valid():
             user = form.save()
             #log the user in
             login(request, user)
             return redirect('curhats:list')
     elif request.method == 'GET':
-        form = UserCreationForm()
-    return render(request, 'index.html', { 'regis_form':form, 'login_form': AuthenticationForm() })
+        form = forms.MyRegis()
+    return render(request, 'index.html', { 'regis_form':form, 'login_form': forms.MyLogin() })
 
 def logout_view(request):
     if request.method == 'POST':
--- curhatpedia/views.py
+++ curhatpedia/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from . import forms
 from django.template.context_processors import csrf
+from accounts import forms
 
 def homepage(request):
-    regis_form = UserCreationForm()
-    login_form = AuthenticationForm()
+    regis_form = forms.MyRegis()
+    login_form = forms.MyLogin()
     return render(request, 'index.html', { 'regis_form': regis_form, 'login_form': login_form })
 
